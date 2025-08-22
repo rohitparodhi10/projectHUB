764fc72df4ac1ba9e0ea66b6236758f5114ddb7e
Rohit Parodhi <rohitparodhi599@gmail.com>
2025-08-22 14:33:56 +0530

    Testing the MMIE error resolve or not

--- projecthub/projecthub/settings.py
+++ projecthub/projecthub/settings.py
@@ -17,8 +17,7 @@ import os
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
+ 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
@@ -132,10 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [
-    BASE_DIR / 'project_front' / 'dist' / 'assets',  # Path to your Vite build output
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
