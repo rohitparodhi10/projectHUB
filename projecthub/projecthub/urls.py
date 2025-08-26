4feb3a89ac1cb7bf8782eb59a14bb69ca810c094
Rohit Parodhi <rohitparodhi599@gmail.com>
2025-08-26 17:16:15 +0530

    Trying to resolve static images error

--- projecthub/projecthub/urls.py
+++ projecthub/projecthub/urls.py
@@ -22,7 +22,7 @@ from feedback.urls import *
 from query.urls import *
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.static import serve
+from django.views.generic.base import RedirectView
 
 import os
 urlpatterns = [
@@ -31,10 +31,8 @@ urlpatterns = [
     path('projectsection/', include("project_section.urls")),
     path('feedback/', include("feedback.urls")),
     path('query/', include("query.urls")),
-    re_path(r'^pictures/(?P<path>.*)$', serve, {
-    'document_root': os.path.join(settings.BASE_DIR, 'static', 'pictures'),
-}),
-
+    re_path(r'^pictures/(?P<path>.*)$',
+            RedirectView.as_view(url='/static/pictures/%(path)s', permanent=True)),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
