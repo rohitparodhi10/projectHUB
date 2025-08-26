40661dd2cb1a1d0ce961b2cc71b5028e706ea2d8
Rohit Parodhi <rohitparodhi599@gmail.com>
2025-08-26 17:07:30 +0530

    changes in urls.py

--- projecthub/projecthub/urls.py
+++ projecthub/projecthub/urls.py
@@ -23,6 +23,7 @@ from query.urls import *
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
+
 import os
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,6 +31,10 @@ urlpatterns = [
     path('projectsection/', include("project_section.urls")),
     path('feedback/', include("feedback.urls")),
     path('query/', include("query.urls")),
+    re_path(r'^pictures/(?P<path>.*)$', serve, {
+    'document_root': os.path.join(settings.BASE_DIR, 'static', 'pictures'),
+}),
+
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
